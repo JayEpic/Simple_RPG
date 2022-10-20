0c5ba4f7fe6ced7a117dedd868f5ebfae0a054b5
JayZn <113024530+JayEpic@users.noreply.github.com>
2022-10-20 21:23:17 +0200

    Images animation character added and some error fixed for walking character

--- entity/Player.java
+++ entity/Player.java
@@ -32,13 +32,17 @@ public class Player extends Entity {
     public void getPlayerImage() {
         try {
             down1 = ImageIO.read(new FileInputStream("res/player/char_walk_down1.png"));
-            down2 = ImageIO.read(new FileInputStream("res/player/char_walk_down2.png"));
-            up1 = ImageIO.read(new FileInputStream("res/player/char_walk_down1.png"));
-            up2 = ImageIO.read(new FileInputStream("res/player/char_walk_down2.png"));
-            left1 = ImageIO.read(new FileInputStream("res/player/char_walk_down1.png"));
-            left2 = ImageIO.read(new FileInputStream("res/player/char_walk_down2.png"));
-            right1 = ImageIO.read(new FileInputStream("res/player/char_walk_down1.png"));
-            right2 = ImageIO.read(new FileInputStream("res/player/char_walk_down2.png"));
+            down2 = ImageIO.read(new FileInputStream("res/player/char_normal.png"));
+            down3 = ImageIO.read(new FileInputStream("res/player/char_walk_down2.png"));
+            up1 = ImageIO.read(new FileInputStream("res/player/char_walk_up1.png"));
+            up2 = ImageIO.read(new FileInputStream("res/player/char_normal_up.png"));
+            up3 = ImageIO.read(new FileInputStream("res/player/char_walk_up2.png"));
+            left1 = ImageIO.read(new FileInputStream("res/player/char_walk_left1.png"));
+            left2 = ImageIO.read(new FileInputStream("res/player/char_normal_left.png"));
+            left3 = ImageIO.read(new FileInputStream("res/player/char_walk_left2.png"));
+            right1 = ImageIO.read(new FileInputStream("res/player/char_walk_right1.png"));
+            right2 = ImageIO.read(new FileInputStream("res/player/char_normal_right.png"));
+            right3 = ImageIO.read(new FileInputStream("res/player/char_walk_right2.png"));
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -68,11 +72,17 @@ public class Player extends Entity {
             }
     
             spriteCounter++;
-            if (spriteCounter > 12) {
+            if (spriteCounter > 10) {
                 if (spriteNum == 1) {
                     spriteNum = 2;
                 }
                 else if(spriteNum == 2) {
+                    spriteNum = 3;
+                }
+                else if(spriteNum == 3) {
+                    spriteNum = 4;
+                }
+                else if(spriteNum == 4) {
                     spriteNum = 1;
                 }
                 spriteCounter = 0;
@@ -90,34 +100,58 @@ public class Player extends Entity {
         switch (direction) {
             case "up":
                 if (spriteNum == 1) {
-                    image = up1;
+                    image = up2;
                 }
                 if (spriteNum == 2) {
+                    image = up3;
+                }
+                if (spriteNum == 3) {
                     image = up2;
+                }
+                if (spriteNum == 4) {
+                    image = up1;
                 }
                 break;
             case "down":
                 if (spriteNum == 1) {
-                    image = down1;
+                    image = down2;
                 }
                 if (spriteNum == 2) {
+                    image = down3;
+                }
+                if (spriteNum == 3) {
                     image = down2;
+                }
+                if (spriteNum == 4) {
+                    image = down1;
                 }
                 break;
             case "left":
                 if (spriteNum == 1) {
-                    image = left1;
+                    image = left2;
                 }
                 if (spriteNum == 2) {
+                    image = left3;
+                }
+                if (spriteNum == 3) {
                     image = left2;
+                }
+                if (spriteNum == 4) {
+                    image = left1;
                 }
                 break;
             case "right":
                 if (spriteNum == 1) {
-                    image = right1;
+                    image = right2;
                 }
                 if (spriteNum == 2) {
+                    image = right3;
+                }
+                if (spriteNum == 3) {
                     image = right2;
+                }
+                if (spriteNum == 4) {
+                    image = right1;
                 }
                 break;
         }
